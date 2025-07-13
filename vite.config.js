2c3844a1dba4885858663e3ff041f9507362d567
David Lewis <mail@david-lewis.com>
2025-07-13 14:24:57 +1000

    feat: add GitHub Actions workflow for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/switcheroo/', // Change to your repo name if different
 })
