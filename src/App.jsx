339f68c5cdb018f80257dfa05477c8b2fee5a83e
David Lewis <mail@david-lewis.com>
2025-07-13 14:32:18 +1000

    feat: add 20 new puzzles with themes and sources to the puzzle set

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 // Hardcoded puzzles for 7 days (famous movie quotes)
 const PUZZLES = [
+  // --- Original 7 puzzles ---
   {
     solution: ["may", "the", "force", "be", "with", "you"],
     scrambled: ["be", "with", "may", "you", "the", "force"],
@@ -52,6 +53,189 @@ const PUZZLES = [
     theme: "Famous Quote",
     source: "John Lennon"
   },
+  // --- 20 new puzzles ---
+  {
+    solution: ["frankly", "my", "dear", "i", "don't", "give", "a", "damn", "about", "anything", "else"],
+    scrambled: ["damn", "anything", "else", "frankly", "my", "dear", "i", "don't", "give", "a", "about"],
+    minSwaps: 5,
+    theme: "Movie Quote",
+    source: "Gone with the Wind"
+  },
+  {
+    solution: ["i", "could", "have", "been", "a", "contender", "i", "could", "have", "been", "somebody"],
+    scrambled: ["been", "i", "could", "have", "been", "somebody", "a", "contender", "i", "could", "have"],
+    minSwaps: 4,
+    theme: "Movie Quote",
+    source: "On the Waterfront"
+  },
+  {
+    solution: ["there's", "no", "place", "like", "home", "when", "you're", "with", "family", "and", "friends"],
+    scrambled: ["home", "when", "you're", "with", "family", "and", "friends", "there's", "no", "place", "like"],
+    minSwaps: 5,
+    theme: "Famous Quote",
+    source: "The Wizard of Oz (paraphrased)"
+  },
+  {
+    solution: ["all", "the", "world's", "a", "stage", "and", "all", "the", "men", "and", "women", "merely", "players"],
+    scrambled: ["players", "all", "the", "world's", "a", "stage", "and", "all", "the", "men", "and", "women", "merely"],
+    minSwaps: 4,
+    theme: "Famous Quote",
+    source: "Shakespeare"
+  },
+  {
+    solution: ["we", "don't", "need", "no", "education", "we", "don't", "need", "no", "thought", "control"],
+    scrambled: ["education", "we", "don't", "need", "no", "thought", "control", "we", "don't", "need", "no"],
+    minSwaps: 4,
+    theme: "Music Lyric",
+    source: "Pink Floyd"
+  },
+  {
+    solution: ["you", "can't", "always", "get", "what", "you", "want", "but", "if", "you", "try", "sometimes"],
+    scrambled: ["want", "but", "if", "you", "try", "sometimes", "you", "can't", "always", "get", "what", "you"],
+    minSwaps: 5,
+    theme: "Music Lyric",
+    source: "The Rolling Stones"
+  },
+  {
+    solution: ["i", "have", "a", "dream", "that", "one", "day", "this", "nation", "will", "rise", "up"],
+    scrambled: ["nation", "will", "rise", "up", "i", "have", "a", "dream", "that", "one", "day", "this"],
+    minSwaps: 4,
+    theme: "Famous Quote",
+    source: "Martin Luther King Jr."
+  },
+  {
+    solution: ["just", "a", "small", "town", "girl", "living", "in", "a", "lonely", "world", "she", "took", "the", "midnight", "train"],
+    scrambled: ["girl", "living", "in", "a", "lonely", "world", "she", "took", "the", "midnight", "train", "just", "a", "small", "town"],
+    minSwaps: 6,
+    theme: "Music Lyric",
+    source: "Journey"
+  },
+  {
+    solution: ["to", "infinity", "and", "beyond", "reach", "for", "the", "sky", "never", "give", "up"],
+    scrambled: ["beyond", "reach", "for", "the", "sky", "never", "give", "up", "to", "infinity", "and"],
+    minSwaps: 5,
+    theme: "Movie Quote",
+    source: "Toy Story (paraphrased)"
+  },
+  {
+    solution: ["i", "get", "by", "with", "a", "little", "help", "from", "my", "friends"],
+    scrambled: ["from", "my", "friends", "i", "get", "by", "with", "a", "little", "help"],
+    minSwaps: 4,
+    theme: "Music Lyric",
+    source: "The Beatles"
+  },
+  {
+    solution: ["the", "greatest", "glory", "in", "living", "lies", "not", "in", "never", "falling", "but", "in", "rising", "every", "time", "we", "fall"],
+    scrambled: ["fall", "the", "greatest", "glory", "in", "living", "lies", "not", "in", "never", "falling", "but", "in", "rising", "every", "time", "we"],
+    minSwaps: 7,
+    theme: "Famous Quote",
+    source: "Nelson Mandela"
+  },
+  {
+    solution: ["i", "will", "survive", "oh", "as", "long", "as", "i", "know", "how", "to", "love", "i", "know", "i'll", "stay", "alive"],
+    scrambled: ["alive", "i", "will", "survive", "oh", "as", "long", "as", "i", "know", "how", "to", "love", "i", "know", "i'll", "stay"],
+    minSwaps: 7,
+    theme: "Music Lyric",
+    source: "Gloria Gaynor"
+  },
+  {
+    solution: ["all", "you", "need", "is", "love", "love", "is", "all", "you", "need"],
+    scrambled: ["love", "is", "all", "you", "need", "all", "you", "need", "is", "love"],
+    minSwaps: 4,
+    theme: "Music Lyric",
+    source: "The Beatles"
+  },
+  {
+    solution: ["i", "am", "the", "master", "of", "my", "fate", "i", "am", "the", "captain", "of", "my", "soul"],
+    scrambled: ["captain", "of", "my", "soul", "i", "am", "the", "master", "of", "my", "fate", "i", "am", "the"],
+    minSwaps: 6,
+    theme: "Famous Quote",
+    source: "William Ernest Henley"
+  },
+  {
+    solution: ["i", "see", "trees", "of", "green", "red", "roses", "too", "i", "see", "them", "bloom", "for", "me", "and", "you"],
+    scrambled: ["bloom", "for", "me", "and", "you", "i", "see", "trees", "of", "green", "red", "roses", "too", "i", "see", "them"],
+    minSwaps: 7,
+    theme: "Music Lyric",
+    source: "Louis Armstrong"
+  },
+  {
+    solution: ["i", "have", "nothing", "to", "offer", "but", "blood", "toil", "tears", "and", "sweat"],
+    scrambled: ["tears", "and", "sweat", "i", "have", "nothing", "to", "offer", "but", "blood", "toil"],
+    minSwaps: 5,
+    theme: "Famous Quote",
+    source: "Winston Churchill"
+  },
+  {
+    solution: ["every", "little", "thing", "is", "gonna", "be", "alright", "don't", "worry", "about", "a", "thing"],
+    scrambled: ["alright", "don't", "worry", "about", "a", "thing", "every", "little", "thing", "is", "gonna", "be"],
+    minSwaps: 5,
+    theme: "Music Lyric",
+    source: "Bob Marley"
+  },
+  {
+    solution: ["the", "future", "belongs", "to", "those", "who", "believe", "in", "the", "beauty", "of", "their", "dreams"],
+    scrambled: ["beauty", "of", "their", "dreams", "the", "future", "belongs", "to", "those", "who", "believe", "in", "the"],
+    minSwaps: 5,
+    theme: "Famous Quote",
+    source: "Eleanor Roosevelt"
+  },
+  {
+    solution: ["i", "walk", "a", "lonely", "road", "the", "only", "one", "that", "i", "have", "ever", "known"],
+    scrambled: ["the", "only", "one", "that", "i", "have", "ever", "known", "i", "walk", "a", "lonely", "road"],
+    minSwaps: 5,
+    theme: "Music Lyric",
+    source: "Green Day"
+  },
+  {
+    solution: ["i", "will", "find", "a", "way", "or", "make", "one", "if", "it", "is", "important", "enough"],
+    scrambled: ["make", "one", "if", "it", "is", "important", "enough", "i", "will", "find", "a", "way", "or"],
+    minSwaps: 5,
+    theme: "Famous Quote",
+    source: "Hannibal (paraphrased)"
+  },
+  {
+    solution: ["i", "get", "knocked", "down", "but", "i", "get", "up", "again", "you're", "never", "gonna", "keep", "me", "down"],
+    scrambled: ["again", "you're", "never", "gonna", "keep", "me", "down", "i", "get", "knocked", "down", "but", "i", "get", "up"],
+    minSwaps: 6,
+    theme: "Music Lyric",
+    source: "Chumbawamba"
+  },
+  {
+    solution: ["the", "only", "limit", "to", "our", "realization", "of", "tomorrow", "will", "be", "our", "doubts", "of", "today"],
+    scrambled: ["our", "doubts", "of", "today", "the", "only", "limit", "to", "our", "realization", "of", "tomorrow", "will", "be"],
+    minSwaps: 5,
+    theme: "Famous Quote",
+    source: "FDR"
+  },
+  {
+    solution: ["i", "want", "to", "break", "free", "i", "want", "to", "ride", "my", "bicycle", "i", "want", "to", "make", "a", "supersonic", "man", "out", "of", "you"],
+    scrambled: ["ride", "my", "bicycle", "i", "want", "to", "make", "a", "supersonic", "man", "out", "of", "you", "i", "want", "to", "break", "free", "i", "want", "to"],
+    minSwaps: 8,
+    theme: "Music Lyric",
+    source: "Queen (combined lyrics)"
+  },
+  {
+    solution: ["i", "have", "not", "failed", "i've", "just", "found", "ten", "thousand", "ways", "that", "won't", "work"],
+    scrambled: ["ways", "that", "won't", "work", "i", "have", "not", "failed", "i've", "just", "found", "ten", "thousand"],
+    minSwaps: 5,
+    theme: "Famous Quote",
+    source: "Thomas Edison"
+  },
+  {
+    solution: ["i", "still", "haven't", "found", "what", "i'm", "looking", "for", "but", "i", "try", "and", "i", "try"],
+    scrambled: ["try", "and", "i", "try", "i", "still", "haven't", "found", "what", "i'm", "looking", "for", "but"],
+    minSwaps: 5,
+    theme: "Music Lyric",
+    source: "U2"
+  },
+  {
+    solution: ["the", "purpose", "of", "our", "lives", "is", "to", "be", "happy", "and", "spread", "joy"],
+    scrambled: ["be", "happy", "and", "spread", "joy", "the", "purpose", "of", "our", "lives", "is", "to"],
+    minSwaps: 4,
+    theme: "Famous Quote",
+    source: "Dalai Lama (paraphrased)"
+  }
 ]
 
 // Pick puzzle based on day of week (0=Sunday)
